c6942669f876ce9fe06103e9ecd3c168d0386c26
InSeongSo <goflvhxj2547@gmail.com>
2022-01-23 22:39:01 +0900

    ✒️Feat: 중앙 저장소에 액션 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,67 +1,79 @@
-// 데이터를 업데이트 할 수 있는 함수
-// + 데이터를 읽어오는 함수
-
-// setMember의 역할 : member set === 추가, 수정, 삭제
-
-// action
-// 추가: name
-// 수정: prevName, nextName
-// 삭제: name
+import { getId } from '../utils/functions.js';
 
 // update 함수를 통했을 당시 값은 전달할 수 있지만, 그 전의 값은 가져오지 못한다.
 const closure = () => {
   // 데이터를 저장할 변수
   const memory = {
-    member: ['파랑', '아벤'],
+    driver: { id: '0', name: '파랑' },
+    navigators: [{ id: '0', name: '아벤' }],
   };
+
+  const subscribers = {};
 
   // 구조분해할당을 통해 변경된 state를 갱신한다.
-  const addMember = (state, { name }) => {
-    state.push(name);
+  const addDriver = (state, { name }) => {
+    state.driver = { ...state.driver, name, id: getId() };
     return state;
   };
-  const modifyMember = (state, { prevName, nextName }) => {};
-  const deleteMember = (state, { name }) => {};
+
+  const addNavigator = (state, { name }) => {
+    state.navigators.push({ name, id: getId() });
+    return state;
+  };
+
+  const deleteDriver = state => {
+    state.driver = {};
+    return state;
+  };
+
+  const deleteNavigator = (state, { id }) => {
+    state.navigators = state.navigators.filter(navigator => navigator.id !== id);
+    return state;
+  };
+
+  const swapRole = (state, { driver, navigator }) => {
+    state.driver = navigator;
+    state.navigators = [...state.navigators.slice(1), driver];
+    return state;
+  };
 
   return action => {
-    if (!action) return memory.member;
+    // 내부 state가 freeze 되어 값 변경이 불가능해짐. 확인 방법은 Object.isFrozen(memory);
+    // if (!action) return Object.freeze(memory);
+    if (!action) return Object.freeze({ ...memory });
     switch (action.type) {
-      case 'addMember': {
-        console.log(`type: ${action.type}\nmember: ${addMember(memory.member, action).join(',')}`);
+      case 'subscribe': {
+        subscribers[action.key] = action.listener;
         break;
       }
-      case 'modifyMember': {
-        console.log(`type: ${action.type}`, modifyMember(memory.member, action));
+      case 'publish': {
+        subscribers[action.key](null, Object.freeze({ ...memory }));
         break;
       }
-      case 'deleteMember': {
-        console.log(`type: ${action.type}`, deleteMember(memory.member, action));
+      case 'addDriver': {
+        console.log(`type: ${action.type}\nmember: ${addDriver(memory, action)}`);
         break;
+      }
+      case 'addNavigator': {
+        console.log(`type: ${action.type}\nmember: ${addNavigator(memory, action)}`);
+        break;
+      }
+      case 'deleteDriver': {
+        console.log(`type: ${action.type}`, deleteDriver(memory));
+        break;
+      }
+      case 'deleteNavigator': {
+        console.log(`type: ${action.type}`, deleteNavigator(memory, action));
+        break;
+      }
+      case 'swapRole': {
+        const state = swapRole(memory, action);
+        console.log(memory);
+        console.log(`type: ${action.type}`, state);
+        return state;
       }
     }
   };
 };
-
-/////////////////////////////////////////
-// ./src/component/modal/Modal.js에서의 호출부
-// import {update} from './src/store/index.js';
-
-/*
-내부 로직 생략
-*/
-
-// action { type, payload }
-// 교통편 : type, 비용: payload
-
-// console.log(memory);
-// update({ type: 'addMember', name: '아벤2' });
-// console.log(memory);
-// update({ type: 'modifyMember', prevName: '아벤2', nextName: '아벤3' });
-// update({ type: 'deleteMember', name: '아벤2' });
-
-// const store = new Store();
-// store.print();
-// store.update({ type: 'addMember', name: '아벤2' });
-// store.print();
 
 export default closure();
